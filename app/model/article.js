8337cc2073ea53671326cf42db0934ac4c5d78b2
jinnianwushuang <35559766+jinnianwushuang@users.noreply.github.com>
2022-04-08 09:55:34 +0800

    d20220408

--- app/model/article.js
+++ app/model/article.js
@@ -1,9 +1,8 @@
 /*
  * @Date           : 2022-03-29 19:29:17
  * @LastEditTime   : 2022-04-03 20:58:09
- * @Description    :  
+ * @Description    :
  */
-
 
 var mongoosePaginate = require("mongoose-paginate");
 module.exports = (app) => {
@@ -11,70 +10,69 @@ module.exports = (app) => {
   const Schema = mongoose.Schema;
 
   const schema_instance = new Schema(
-  
     {
-      
       //作者ID
-       author: { type: String, required: true },
-        //时刻内容  
-        moment_content: { type: String, required: false },
-        // 图片 数组
-        moment_images: [String],
-        //常规内容    [  { type:'text',content:""},  { type:'video',content:""} , { type:'image',content:""}  ]
-        common_content: [{}],
-         // 视频地址
-        video_src:  String,
-        //常规内容    [  { type:'text',content:""},  { type:'video',content:""} , { type:'image',content:""}  ]
-        video_content: { type: String, required: false },
+      author: { type: String, required: true },
+      //时刻内容
+      moment_content: { type: String, required: false },
+      // 图片 数组
+      moment_images: [String],
+      //常规内容    [  { type:'text',content:""},  { type:'video',content:""} , { type:'image',content:""}  ]
+      common_content: { type: String, required: false },
+      //常规标题
+      common_title: { type: String, required: false },
+
+      // 视频地址
+      video_src: String,
+      //常规内容    [  { type:'text',content:""},  { type:'video',content:""} , { type:'image',content:""}  ]
+      video_content: { type: String, required: false },
       //类型   1 momentArticle  时刻文章  2 videoArticle   视频文章  3   commonArticle 常规文章
-      type:{
-        type:Number,
-        required: true
+      type: {
+        type: Number,
+        required: true,
       },
 
       //官方助手
-      updateRecord:[Number],
+      assistant: [Number],
       //话题
-      topic:[Number],
+      topic: [Number],
 
       //状态
-      status:{
-        type:Number,
-         // -1 待审核  ， 1 启用 ，  -2,禁用 ，3 热推 
-        default:-1
-      } ,
-      // 是否置顶 
-      is_top :{
-        type: Boolean,
-        default: false
+      status: {
+        type: Number,
+        // -1 关闭  ， 1 启用 ，  -2,禁用 ，3 待审核  4 热推
+        default: -1,
       },
-      // 是否置顶 
-      is_top :{
+      // 是否置顶
+      is_top: {
         type: Boolean,
-        default: false
+        default: false,
       },
-       // 是否 精华
-       is_best :{
+      // 是否置顶
+      is_top: {
         type: Boolean,
-        default: false
+        default: false,
       },
-
+      // 是否 精华
+      is_best: {
+        type: Boolean,
+        default: false,
+      },
 
       //点赞
-      like:{
-          type:Number,
-          default:0
+      like: {
+        type: Number,
+        default: 0,
       },
       //精选置顶 评论
-      top_comment:String,
+      top_comment: String,
       // 评论
-      comment:{
-        type:Number,
-        default:0
+      comment: {
+        type: Number,
+        default: 0,
       },
-      //最后一次评论 
-      last_comment_time:Date
-       
+      //最后一次评论
+      last_comment_time: Date,
     },
     {
       timestamps: true,
